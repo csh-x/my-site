03d7bef8e6066bf3b692589550dcd976ce4a8747
joyboy <shiyiersan23@163.com>
2023-10-31 22:11:49 +0800

    初始提交

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: csh
+ * @Date: 2023-10-17 22:00:01
+ * @LastEditors: csh
+ * @LastEditTime: 2023-10-18 11:18:50
+ * @FilePath: /my-site/vue.config.js
+ * @Description: 
+ */
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
