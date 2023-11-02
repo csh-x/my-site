5cc43ec42d68f3d8de025afb4bc40d9b37f670bf
JoyBoy <shiyiersan23@163.com>
2023-11-02 10:18:09 +0800

    封装icon、avatar组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-02 09:12:59
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-02 10:05:15
+ * @FilePath: /my-site/src/components/Icon.vue
+ * @Description: 
+-->
+<template>
+  <i class="iconfont" :class="fontClass"></i>
+</template>
+
+<script>
+const classMap = {
+    home: 'icon-home',
+    user: 'icon-yonghuguanli',
+    login: 'icon-denglurizhi'
+}
+export default {
+    props: {
+        type: {
+            type: String,
+            required: true,
+        }
+    },
+    computed: {
+        fontClass() {
+            return classMap[this.type]
+        }
+    }
+}
+</script>
+
+<style scoped>
+    @import "//at.alicdn.com/t/c/font_4312462_o381y74zahd.css";
+    .iconfont {
+        color: inherit;
+        font-size: inherit;
+    }
+</style>
