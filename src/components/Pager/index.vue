8d2c3b1a0659dec0d8ebf5799e3af25ae6800cee
JoyBoy <shiyiersan23@163.com>
2023-11-03 23:42:40 +0800

    Pager组件封装

---
+++ src/components/Pager/index.vue
@@ -0,0 +1,102 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-02 18:45:28
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-03 08:39:37
+ * @FilePath: /my-site/src/components/Pager/index.vue
+ * @Description: 
+-->
+<template>
+    <div class="pager-container" v-if="pagerNumber > 1">
+        <a @click="handleClick(1)" :class="{ disabled: current === 1 }">|&lt;&lt;</a>
+        <a @click="handleClick(current - 1)" :class="{ disabled: current === 1 }">&lt;&lt;</a>
+        <a @click="handleClick(n)" v-for="(n, i) in numbers" :key="i" :class="{ active: n === current }">{{ n }}</a>
+        <a @click="handleClick(current + 1)" :class="{ disabled: current === pagerNumber }">&gt;&gt;</a>
+        <a @click="handleClick(pagerNumber)" :class="{ disabled: current === pagerNumber }">&gt;&gt;|</a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        current: {
+            type: Number,
+            default: 1,
+        },
+        total: {
+            type: Number,
+            default: 0,
+        },
+        limit: {
+            type: Number,
+            default: 10,
+        },
+        visibleNmber: {
+            type: Number,
+            default: 10,
+        },
+    },
+    computed: {
+        pagerNumber() {
+          return Math.ceil(this.total / this.limit)
+        },
+        // 得到显示的最小数字  
+        visibleMin() {
+          let min = this.current - Math.floor(this.visibleNmber / 2)
+          min < 1 && (min = 1)
+          return min
+        },
+        visibleMax() {
+            let max = this.visibleMin + this.visibleNmber - 1
+            max > this.pagerNumber && (max = this.pagerNumber)
+            return max
+        },
+        numbers() {
+          let nums = []
+          for(let i = this.visibleMin; i <= this.visibleMax; i++) {
+            nums.push(i) 
+          }
+          return nums
+        }
+    },
+    methods: {
+      handleClick(newPage) {
+        if(newPage < 1) {
+            newPage = 1
+        }
+        if(newPage > this.pagerNumber) {
+            newPage = this.pagerNumber
+        }
+        if(newPage === this.current) {
+            return 
+        }
+        this.$emit("pageChange", newPage)
+      }
+
+    },
+};
+</script> 
+
+<style lang="less" scoped>
+@import "~@/styles/var.less";
+.pager-container {
+    display: flex;
+    justify-content: center;
+    margin: 20px 0;
+
+    a {
+        color: @primary;
+        margin: 0 6px;
+        cursor: pointer;
+
+        &.disabled {
+            color: @gray;
+            cursor: not-allowed;
+        }
+
+        &.active {
+            color: @dark;
+        }
+    }
+}
+</style>
---
+++ src/components/Pager/test.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-03 08:40:00
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-03 08:41:23
+ * @FilePath: /my-site/src/components/Pager/test.vue
+ * @Description: 
+-->
+<template>
+    <div id="app">
+        <h1>App组件</h1>
+        <Pager @pageChange="handlePageChange" :total="total" :current="current" :visibleNmber="visibleNmber"/>
+    </div>
+</template>
+
+<script>
+import Pager from "./"
+
+export default {
+    name: "App",
+    components: {
+      Pager
+    },
+    data() {
+        return {
+            total: 300,
+            visibleNmber: 15,
+            current: 13,
+        };
+    },
+    methods: {
+        handlePageChange(newPage) {
+            console.log(newPage);
+            this.current = newPage
+        }
+    }
+};
+</script>
+
+<style scoped>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.iconfont {
+  color: red;
+}
+</style>
