416a9863124e32c7f2fe095ca1aaf6e01c846e93
JoyBoy <shiyiersan23@163.com>
2023-11-06 00:16:33 +0800

    empty组件

---
+++ src/components/Empty/index.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-05 14:42:28
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-06 00:14:33
+ * @FilePath: /my-site/src/components/Empty/index.vue
+ * @Description: 
+-->
+<template> 
+    <div class="empty-container">
+        <Icon type="empty" />
+        <p>{{ text }}</p>
+    </div>
+</template>
+
+<script>
+import Icon from "../Icon";
+export default {
+    components: { Icon }, 
+    props: {
+        text: {
+            type: String,
+            default: "无数据",
+        },
+    }, 
+    data() {
+        return {
+        };
+    },
+};
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/mixin.less";
+@import "~@/styles/var.less";
+
+.empty-container {
+    .abs-center();
+    color: @gray;
+    text-align: center;
+
+    .icon-container {
+        font-size: 72px;
+    }
+}
+</style>
---
+++ src/components/Empty/test.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-05 14:42:28
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-06 00:06:25
+ * @FilePath: /my-site/src/components/Empty/test.vue
+ * @Description: Empty组件
+-->
+<template>
+    <div class="test-container">
+        <Empty />
+    </div>
+</template>
+
+<script>
+import Empty from "./";
+export default {
+    components: { Empty },
+};
+</script>
+
+<style lang="less" scoped>
+    .test-container {
+        width: 500px;
+        height: 300px;
+        border: 1px solid #ff00ed;
+        position: relative;
+    }
+</style>
