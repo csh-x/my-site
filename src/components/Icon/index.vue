0939948b903e054ddee34f969dc388a1d44eb0a7
JoyBoy <shiyiersan23@163.com>
2023-11-05 23:59:23 +0800

    icon

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -2,40 +2,56 @@
  * @Author: csh
  * @Date: 2023-11-02 09:12:59
  * @LastEditors: csh
- * @LastEditTime: 2023-11-04 21:52:55
+ * @LastEditTime: 2023-11-05 16:17:46
  * @FilePath: /my-site/src/components/Icon/index.vue
  * @Description: 
 -->
 <template>
     <i class="iconfont icon-container" :class="fontClass"></i>
-  </template>
+</template>
   
   <script>
-  const classMap = {
-      home: 'icon-home',
-      user: 'icon-yonghuguanli',
-      login: 'icon-denglurizhi'
-  } 
-  export const types = Object.keys(classMap)
-  export default {
-      props: {
-          type: {
-              type: String,
-              required: true,
-          }
-      },
-      computed: {
-          fontClass() {
-              return classMap[this.type]
-          }
-      }
-  }
-  </script>
+const classMap = {
+    home: "icon-home",
+    user: "icon-yonghuguanli",
+    login: "icon-denglurizhi",
+    empty: "icon-empty",
+    success: "iconzhengque",
+    error: "iconcuowu",
+    close: "iconguanbi",
+    warn: "iconjinggao",
+    info: "iconxinxi",
+    blog: "iconblog",
+    code: "iconcode",
+    about: "iconset_about_hov",
+    weixin: "iconweixin",
+    mail: "iconemail",
+    github: "icongithub",
+    qq: "iconsign_qq",
+    arrowUp: "iconiconfonticonfonti2copy",
+    arrowDown: "iconiconfonticonfonti2",
+    chat: "iconliuyan",
+};
+export const types = Object.keys(classMap);
+export default {
+    props: {
+        type: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        fontClass() {
+            return classMap[this.type];
+        },
+    },
+};
+</script>
   
   <style scoped>
-      @import "//at.alicdn.com/t/c/font_4312462_o381y74zahd.css";
-      .iconfont {
-          color: inherit;
-          font-size: inherit;
-      }
-  </style>
+@import "//at.alicdn.com/t/c/font_4312462_n7exqe3nn1.css";
+.iconfont {
+    color: inherit;
+    font-size: inherit;
+}
+</style>
