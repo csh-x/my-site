e035f75d9a529ac33834d7d1a8e3c565f78fb5a1
JoyBoy <shiyiersan23@163.com>
2023-11-08 22:04:42 +0800

    Menu菜单初始化提交，烦死了今天工作debug到崩溃，没心情写了。。

---
+++ src/components/Menu/index.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-07 20:25:38
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-08 22:03:31
+ * @FilePath: /my-site/src/components/Menu/index.vue
+ * @Description: 
+-->
+<template>
+  <div class="menu-container">
+    <a href="">
+        <Icon type="home" />
+    </a>
+    <span>首页</span>
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/Icon"
+export default {
+    components: {
+        Icon
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/global.less";
+@import "~@/styles/var.less";
+    .menu-container {
+        color: @gray;
+    }
+</style>
---
+++ src/components/Menu/test.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-06 22:01:37
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-07 20:28:11
+ * @FilePath: /my-site/src/components/Menu/test.vue
+ * @Description: 
+-->
+<template>
+    <div class="test-container">
+        <Menu />
+    </div>
+</template>
+
+<script>
+import Menu from "./";
+
+export default {
+    components: {
+        Menu,
+    },
+    methods: {
+    }
+};
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/global.less";
+
+.test-container {
+    width: 400px;
+    height: 600px;
+    border: 1px solid red;
+}
+</style>
