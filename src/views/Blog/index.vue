f2bece4d042844c3b9660e6cf54ebc1828925a84
shiyiersan@foxmail.com <shiyiersan@foxmail.com>
2023-11-23 22:41:32 +0800

    router的初始页面结构

---
+++ src/views/Blog/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-23 22:37:44
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-23 22:39:44
+ * @FilePath: /csh-plugin/Users/csh/my-site/src/views/Blog/index.vue
+ * @Description: 博客页
+-->
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: csh
+ * @Date: 2023-11-23 22:37:26
+ * @LastEditors: csh
+ * @LastEditTime: 2023-11-23 22:39:47
+ * @FilePath: /csh-plugin/Users/csh/my-site/src/views/Home/index.vue
+ * @Description: 主页
+-->
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
